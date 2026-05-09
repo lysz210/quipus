4d73cd5627bcfae5071d4a0ef13ded5c05297e2f
Lingyong Sun <lingyong.sun@gmail.com>
2026-05-09 19:36:51 +0200

    feat: convert into kotlin project

---
+++ credentials/build.gradle.kts
@@ -0,0 +1,33 @@
+import com.google.protobuf.gradle.id
+
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+    // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
+    alias(libs.plugins.kotlinPluginSerialization)
+    id("com.google.protobuf") version "0.10.0"
+}
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-kotlin:${properties["protobuf.Version"]}")
+    // Apply the kotlinx bundle of dependencies from the version catalog (`gradle/libs.versions.toml`).
+    implementation(libs.bundles.kotlinxEcosystem)
+    testImplementation(kotlin("test"))
+}
+
+protobuf {
+    // Configure the protoc executable
+    protoc {
+        artifact = "com.google.protobuf:protoc:${properties["protobuf.Version"]}"
+    }
+
+    // Generates Kotlin extensions in addition to Java code
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}

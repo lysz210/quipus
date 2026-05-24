14bca79d5ebffd16b0a77b7c9865dde012096529
Lingyong Sun <lingyong.sun@gmail.com>
2026-05-24 12:43:26 +0200

    feat: add module maps

---
+++ maps/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    // Apply the shared build logic from a convention plugin.
+    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
+    id("buildsrc.convention.kotlin-jvm")
+    // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
+    alias(libs.plugins.kotlinPluginSerialization)
+    id("com.google.protobuf") version "0.10.0"
+}
+
+dependencies {
+    api("com.google.protobuf:protobuf-kotlin:${properties["protobuf.Version"]}")
+    // Apply the kotlinx bundle of dependencies from the version catalog (`gradle/libs.versions.toml`).
+    implementation(libs.bundles.kotlinxEcosystem)
+    testImplementation(kotlin("test"))
+    testImplementation(platform("org.assertj:assertj-bom:${properties["assertj.BomVersion"]}"))
+    testImplementation("org.assertj:assertj-core")
+}
+
+protobuf {
+    // Configure the protoc executable
+    protoc {
+        artifact = "com.google.protobuf:protoc:${properties["protobuf.Version"]}"
+    }
+
+    // Generates Kotlin extensions in addition to Java code
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                create("kotlin")
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,3 +19,4 @@ plugins {
 
 rootProject.name = "quipus"
 include(":credentials")
+include(":maps")

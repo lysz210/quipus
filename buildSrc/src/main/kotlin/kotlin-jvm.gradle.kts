48b465f20a0853bf173fd5d78e062025943c3511
Lingyong Sun <lingyong.sun@gmail.com>
2026-06-01 12:32:49 +0200

    chore: init github workflow

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -45,4 +45,15 @@ publishing {
             version = project.version.toString()
         }
     }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
